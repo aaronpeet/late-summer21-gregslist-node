3ed0bace3932eaa0b5b2da821113ea25e495e31d
aaronpeet <aaronpeet@icloud.com>
2021-07-27 16:48:57 -0600

    finished homes

--- server/controllers/HomesController.js
+++ server/controllers/HomesController.js
@@ -21,4 +21,52 @@ export class HomesController extends BaseController {
       next(error)
     }
   }
+
+  async getById(req, res, next) {
+    try {
+      const home = await homesService.getById(req.params.id)
+      res.send(home)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const home = homesService.create(req.body)
+      res.send(home)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      res.body.id = res.params.id
+      delete req.body.price
+      const home = homesService.edit(req.body)
+      res.send(home)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async bid(req, res, next) {
+    try {
+      const bid = { price: req.body.price, id: req.params.id }
+      const home = await homesService.bid(bid)
+      res.send(home)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async destroy(req, res, next) {
+    try {
+      await homesService.destroy(req.params.id)
+      res.send({ message: 'Successfully Deleted Home' })
+    } catch (error) {
+      next(error)
+    }
+  }
 }
--- server/services/HomesService.js
+++ server/services/HomesService.js
@@ -6,6 +6,44 @@ class HomesService {
     const homes = await dbContext.Homes.find(query)
     return homes
   }
+
+  async getById(id) {
+    const home = await dbContext.Homes.findById(id)
+    if (!home) {
+      throw new BadRequest('Invalid ID')
+    }
+    return home
+  }
+
+  async create(body) {
+    const home = await dbContext.Homes.create(body)
+    return home
+  }
+
+  async edit(body) {
+    const home = await dbContext.Homes.findByIdAndUpdate(body.id, body, { new: true, runValidators: true })
+    if (!home) {
+      throw new BadRequest('Invalid ID')
+    }
+    return home
+  }
+
+  async bid(body) {
+    let home = await this.getById(body.id)
+    if (home.price > body.price) {
+      throw new BadRequest('Homes can only be bid up.')
+    }
+    home = await dbContext.Homes.findByIdAndUpdate(body.id, body, { new: true, runValidators: true })
+    return home
+  }
+
+  async destroy(id) {
+    const home = await dbContext.Homes.findByIdAndDelete(id)
+    if (!home) {
+      throw new BadRequest('Invalid ID')
+    }
+    return home
+  }
 }
 
 export const homesService = new HomesService()
